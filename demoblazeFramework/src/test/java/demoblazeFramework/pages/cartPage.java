9bd0afc9274daf7010351772819e6476b589cea6
perum <perum@LAPTOP-N7NAH9MI>
2025-02-28 18:47:08 +0530

    Demoblaze Framework commited and pushed

---
+++ demoblazeFramework/src/test/java/demoblazeFramework/pages/cartPage.java
@@ -0,0 +1,34 @@
+package demoblazeFramework.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+public class cartPage {
+
+	WebDriver driver;
+
+	public cartPage(WebDriver ldriver) {
+
+		this.driver = ldriver;
+	}
+
+	@FindBy(xpath = "//tr[@class='success']//td[text()='Samsung galaxy s6']")
+	WebElement verifySamsungGalaxyProduct;
+
+	@FindBy(xpath = "//tr[@class='success']//a[text()='Delete']")
+	WebElement deleteSamsungGalaxyProduct;
+
+	public void verifySamsunProductInCart() {
+
+		Assert.assertTrue(verifySamsungGalaxyProduct.isDisplayed(), "Samsung Galaxy S6 is not present in the cart");
+	}
+
+	public void deleteSamsunProductFromCart() {
+
+		deleteSamsungGalaxyProduct.click();
+
+	}
+
+}
